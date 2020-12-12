83e07089cf20ab5f8cad1b9b58736066e5dc432e
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-12-12 17:44:30 +0530

    Update locks.py

--- ItachiRobot/modules/locks.py
+++ ItachiRobot/modules/locks.py
@@ -19,10 +19,10 @@ from SaitamaRobot.modules.helper_funcs.chat_status import (
     is_bot_admin,
     user_admin,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.connection import connected
+from ItachiRobot.modules.log_channel import loggable
+from ItachiRobot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from ItachiRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
