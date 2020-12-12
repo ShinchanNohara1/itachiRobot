c7fa9bfc03aa3f55d2dcb58d6072bdbaaea0ac09
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-12-12 17:28:27 +0530

    Update warns.py

--- ItachiRobot/modules/warns.py
+++ ItachiRobot/modules/warns.py
@@ -3,21 +3,21 @@ import re
 from typing import Optional
 
 import telegram
-from SaitamaRobot import TIGERS, WOLVES, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin,
+from ItachiRobot import TIGERS, WOLVES, dispatcher
+from ItachiRobot.modules.disable import DisableAbleCommandHandler
+from ItachiRobot.modules.helper_funcs.chat_status import (bot_admin,
                                                            can_restrict,
                                                            is_user_admin,
                                                            user_admin,
                                                            user_admin_no_reply)
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_text,
+from ItachiRobot.modules.helper_funcs.extraction import (extract_text,
                                                           extract_user,
                                                           extract_user_and_text)
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.helper_funcs.string_handling import split_quotes
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql import warns_sql as sql
+from ItachiRobot.modules.helper_funcs.filters import CustomFilters
+from ItachiRobot.modules.helper_funcs.misc import split_message
+from ItachiRobot.modules.helper_funcs.string_handling import split_quotes
+from ItachiRobot.modules.log_channel import loggable
+from ItachiRobot.modules.sql import warns_sql as sql
 from telegram import (CallbackQuery, Chat, InlineKeyboardButton,
                       InlineKeyboardMarkup, Message, ParseMode, Update, User)
 from telegram.error import BadRequest
