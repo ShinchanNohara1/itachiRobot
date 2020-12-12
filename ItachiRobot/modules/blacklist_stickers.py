2a4c7b4dd71315ec2dcdbd1c378ddbb2e6685bfc
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-12-12 17:57:17 +0530

    Update blacklist_stickers.py

--- ItachiRobot/modules/blacklist_stickers.py
+++ ItachiRobot/modules/blacklist_stickers.py
@@ -1,18 +1,18 @@
 import html
 from typing import Optional
 
-import SaitamaRobot.modules.sql.blsticker_sql as sql
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
-from SaitamaRobot.modules.helper_funcs.chat_status import (user_admin,
+import ItachiRobot.modules.sql.blsticker_sql as sql
+from ItachiRobot import LOGGER, dispatcher
+from ItachiRobot.modules.connection import connected
+from ItachiRobot.modules.disable import DisableAbleCommandHandler
+from ItachiRobot.modules.helper_funcs.alternate import send_message
+from ItachiRobot.modules.helper_funcs.chat_status import (user_admin,
                                                            user_not_admin)
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
+from ItachiRobot.modules.helper_funcs.misc import split_message
+from ItachiRobot.modules.helper_funcs.string_handling import extract_time
 
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.warns import warn
+from ItachiRobot.modules.log_channel import loggable
+from ItachiRobot.modules.warns import warn
 from telegram import (Chat, Message, ParseMode, Update, User, ChatPermissions)
 from telegram.error import BadRequest
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
