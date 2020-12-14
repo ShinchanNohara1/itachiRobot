b2088624aa41fbcf8d5512b4e39176db8461ca14
Captain levi <67622043+Tusharpalx@users.noreply.github.com>
2020-12-14 14:13:04 +0530

    update __init__.py

--- ItachiRobot/__init__.py
+++ ItachiRobot/__init__.py
@@ -184,7 +184,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
+from ItachiRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
